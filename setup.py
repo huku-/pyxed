5682c6ec599ad387c484fa6727ed83d3a074a3cd
William Woodruff <william@trailofbits.com>
2020-04-16 13:09:47 -0400

    setup: Re-add extra_compile_args with -fPIC

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ except OSError as e:
 # Set up our sources, libraries, and includes for compilation
 pyxed_mod = Extension('pyxed',
                       include_dirs=[os.path.join(XED_CURRENT_KIT_DIR, "include/xed")],
+                      extra_compile_args=["-fPIC"],
                       extra_link_args=['-Wl,--whole-archive',
                                        os.path.join(XED_CURRENT_KIT_DIR, "lib/libxed.a"),
                                        '-Wl,--no-whole-archive'],
